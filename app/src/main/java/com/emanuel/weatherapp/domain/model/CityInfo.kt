7d8f67f8db5657c2720fc7b2b6b7192cba33c6a1
Emanuel Rodrigues <emanuel_bim@hotmail.com>
2024-11-11 09:28:06 -0300

    config default values

--- app/src/main/java/com/emanuel/weatherapp/domain/model/CityInfo.kt
+++ app/src/main/java/com/emanuel/weatherapp/domain/model/CityInfo.kt
@@ -1,7 +1,7 @@
 package com.emanuel.weatherapp.domain.model
 
 data class CityInfo(
-    val name: String,
-    val lat: Float,
-    val lon: Float
+    val name: String = "",
+    val lat: Float = 0.0f,
+    val lon: Float = 0.0f
 )
