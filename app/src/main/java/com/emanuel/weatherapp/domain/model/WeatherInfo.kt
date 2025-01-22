d392ab79180a3fb3286943f765e4e568772e04d5
Emanuel Rodrigues <emanuel_bim@hotmail.com>
2025-01-22 11:17:16 -0300

    change data class to parcelable

--- app/src/main/java/com/emanuel/weatherapp/domain/model/WeatherInfo.kt
+++ app/src/main/java/com/emanuel/weatherapp/domain/model/WeatherInfo.kt
@@ -1,10 +1,14 @@
 package com.emanuel.weatherapp.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class WeatherInfo(
-    val cityName: String,
-    val dayOfWeek: String,
-    val currentDate: String,
-    val temperature: Int,
-    val climate: String,
-    val humidity: Int,
-)
+    val cityName: String = "",
+    val dayOfWeek: String = "",
+    val currentDate: String = "",
+    val temperature: Int = 0,
+    val climate: String = "",
+    val humidity: Int = 0,
+) : Parcelable
