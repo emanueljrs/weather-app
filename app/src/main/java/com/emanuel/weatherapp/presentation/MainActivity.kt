4168583b84e3f94e8ad7f569566b2d114c0d21e0
Emanuel Rodrigues <emanuel_bim@hotmail.com>
2025-01-31 09:22:11 -0300

    remove unnecessary viewModel

--- app/src/main/java/com/emanuel/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/emanuel/weatherapp/presentation/MainActivity.kt
@@ -2,13 +2,11 @@ package com.emanuel.weatherapp.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.emanuel.weatherapp.R
-import com.emanuel.weatherapp.presentation.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
